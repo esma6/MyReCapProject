b85352698407ffa67ff8ba86fcf19503edf741e5
esma6 <77541320+esma6@users.noreply.github.com>
2021-02-19 14:23:49 +0300

    kamp 12.gün ödevi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.Concrete.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,14 +23,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
-        {
-
-            if (car.CarName.Length<2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-
-            }
+        { 
            
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.CarName).MinimumLength(2);
+            RuleFor(c => c.CarName).Must(StartWithA).WithMessage("Araç ismi A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
